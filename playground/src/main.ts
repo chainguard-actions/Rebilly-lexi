e55ba87a265a69438fab723846a7eed323ec11aa
actionchain <actionchain@chainguard.dev>
2026-06-15 18:59:53 +0000

    Update Rebilly/lexi@v2.3.4

---
+++ playground/src/main.ts
@@ -0,0 +1,10 @@
+import {createApp} from 'vue';
+import {install as VueMonacoEditorPlugin} from '@guolao/vue-monaco-editor';
+// @ts-expect-error
+import revel from '@rebilly/revel';
+import './style.css';
+import App from './App.vue';
+
+import '@rebilly/revel/dist/style.css';
+
+createApp(App).use(revel).use(VueMonacoEditorPlugin).mount('#app');
