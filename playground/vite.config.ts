8c5233d0f45f9466f76ecd57ed274af7788dc005
actionchain <actionchain@chainguard.dev>
2026-06-16 00:20:16 +0000

    Update Rebilly/lexi@v2.3.6

---
+++ playground/vite.config.ts
@@ -0,0 +1,18 @@
+import {defineConfig} from 'vite';
+import vue from '@vitejs/plugin-vue';
+import mkcert from 'vite-plugin-mkcert';
+
+export default defineConfig({
+    root: './playground',
+    // Set the base path, without this vite will reference the js and asset
+    // files as "/etc/blah.js" which will break the page if hosted at a sub
+    // path such as www.example.com/lexi/
+    base: './',
+    plugins: [vue(), mkcert()],
+    build: {
+        cssMinify: false,
+    },
+    server: {
+        https: true,
+    },
+});
